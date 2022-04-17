30711553fa89b83721c7fd172a4df85fc21c133e
theophildio <dtheo.sangma@gmail.com>
2022-04-17 18:19:16 +0600

    Features section added in Home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Features from './Features/Features';
 import './Home.css';
 
 const Home = () => {
   return (
     <main className='home'>
       <Banner></Banner>
+      <Features></Features>
     </main>
   );
 };
