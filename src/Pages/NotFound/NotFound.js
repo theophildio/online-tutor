2467775b224d8fea3d84738017a462455edf35aa
theophildio <dtheo.sangma@gmail.com>
2022-04-19 21:14:31 +0600

    404 page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './NotFound.css';
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div style={{textAlign: 'center'}}>
-      <h1>404</h1>
-      <h3>Page not found</h3>
+    <div className='not-found'>
+      <h1>Oops!</h1>
+      <h3>404 - Page not found</h3>
+      <p>The page you are looking for might have been removed <br /> had its name changed or is temporarily unavaiable</p>
+      <button onClick={() => navigate('/')} className="tutor-btn">go to homepage</button>
     </div>
   );
 };
